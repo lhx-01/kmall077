2dfa894b6a748965a6d20074a368614b5da90900
lihexiang_glb <lihexiang_glb@kgc.com>
2020-12-15 16:23:44 +0800

    测试成功

---
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
@@ -0,0 +1,25 @@
+package com.kgc.kmall.user.service;
+
+import com.kgc.kmall.bean.Member;
+import com.kgc.kmall.service.MemberService;
+import com.kgc.kmall.user.mapper.MemberMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author LHX
+ * @Date 2020/12/15 16:10
+ */
+@Service
+public class MemberServiceImpl implements MemberService {
+    @Resource
+    MemberMapper memberMapper;
+
+    @Override
+    public List<Member> selectAllMember() {
+        List<Member> members = memberMapper.selectByExample(null);
+        return members;
+    }
+}
---
+++ kmallapi/src/main/java/com/kgc/kmall/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.service;
+
+import com.kgc.kmall.bean.Member;
+
+import java.util.List;
+
+/**
+ * @Author LHX
+ * @Date 2020/12/15 16:10
+ */
+public interface MemberService {
+    public List<Member> selectAllMember();
+}
