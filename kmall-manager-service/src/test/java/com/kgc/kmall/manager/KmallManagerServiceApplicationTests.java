8deebcac3ac9bb38923fbee9fd5a238c2f1cac5d
lihexiang_glb <lihexiang_glb@kgc.com>
2020-12-16 15:31:10 +0800

    kmall-manager-service测试一级查询

---
+++ kmall-manager-service/src/test/java/com/kgc/kmall/manager/KmallManagerServiceApplicationTests.java
@@ -0,0 +1,24 @@
+package com.kgc.kmall.manager;
+
+import com.kgc.kmall.bean.PmsBaseCatalog1;
+import com.kgc.kmall.service.CatalogService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class KmallManagerServiceApplicationTests {
+	@Resource
+	CatalogService catalogService;
+
+	@Test
+	void contextLoads() {
+		List<PmsBaseCatalog1> catalog1s=catalogService.getCatalog1();
+		for (PmsBaseCatalog1 catalog1 : catalog1s){
+            System.out.println(catalog1);
+        }
+	}
+
+}
